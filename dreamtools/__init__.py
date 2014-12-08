69def2e191971f9c855cb8b16d89cd32bce35f4a
Thomas Cokelaer <cokelaer@gmail.com>
2014-12-08 18:10:23 +0000

    fixing https://github.com/dreamtools/dreamtools/issues/6

--- dreamtools/__init__.py
+++ dreamtools/__init__.py
@@ -13,12 +13,14 @@ except:
 def create_dreamtools_config_directories(maindir, subdirectories):
     import os
     cfg = configuration.user_config_dir
+    print("Creating directories in %s for %s" % (cfg, maindir) )
     path = cfg + os.sep + 'data' + os.sep + maindir
     if os.path.isdir(path) is False:
         os.mkdir(path)
-        for directory in subdirectories:
-            subdir = path + os.sep +directory
-            if os.path.isdir(subdir):
-                os.mkdir(path)
+    for directory in subdirectories:
+        subdir = path + os.sep +directory
+        if os.path.isdir(subdir) is False:
+            print(" - creating %s" % subdir)
+            os.mkdir(subdir)
 
 
