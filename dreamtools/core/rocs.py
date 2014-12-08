288ba06a3aaf54d7f21d0ed3f4733180bc835121
cokelaer <cokelaer@gmail.com>
2014-12-08 13:29:51 +0000

    add modules.

---
+++ dreamtools/core/rocs.py
@@ -0,0 +1,228 @@
+# -*- python -*-
+#
+#  This file is part of DreamTools software
+#
+#  Copyright (c) 2011-2012 - EBI-EMBL
+#
+#  File author(s): Thomas Cokelaer <cokelaer@ebi.ac.uk>
+#
+#  Distributed under the GPLv3 License.
+#  See accompanying file LICENSE.txt or copy at
+#      http://www.gnu.org/licenses/gpl-3.0.html
+#
+#  website: 
+#
+##############################################################################
+"""Provides tools related to Receiver Operating Characteristic (ROC)"""
+import numpy as np
+
+
+class ROC(object):
+    """A class to compute ROC, AUC and AUPRs
+
+
+        >>> r = ROC() # could provide scores and labels as arguments
+        >>> r.scores = [0.9,0.8,0.7,.6,.6,.3]
+        >>> r.classes = [1,0,1,0,1,1]
+        >>> r.compute_auc()
+        0.4375
+    
+
+    """
+    def __init__(self, scores=None, classes=None):
+        """:Constructor:
+
+        :param list scores: the scores
+        :param list classes: binary class made of 1 or 0 numerical values. Also
+            called labels in the literature.
+        """
+        self._scores = None
+        self._classes = None
+
+        self.scores = scores
+        self.classes = classes
+
+    def _set_scores(self, values):
+        self._scores = values
+    def _get_scores(self):
+        return self._scores
+    scores = property(_get_scores, _set_scores, doc="Read/Write the scores")
+
+    def _set_classes(self, values):
+        self._classes = values
+    def _get_classes(self):
+        return self._classes
+    classes = property(_get_classes, _set_classes, doc="Read/Write the classes")
+
+    def _compute_fpr_tpr(self, FP, TP, N, P):
+        roc = {"fpr":[], "tpr":[], "FP":[], "TP":[]}
+        roc['fpr'] = FP/float(N)
+        roc['tpr'] = TP/float(P)
+        roc['TP'] = TP
+        roc['FP'] = FP
+        return roc
+
+    def get_roc(self):
+        """Compute the ROC curve X/Y vectors and some other metrics
+
+        :return: a dictionary with different metrics such FPR (false positive
+            rate), PTR (true positive rate).
+
+        """
+        scores = self.scores
+        classes = list(self.classes)
+
+        sorted_indices = np.argsort(1-np.array(scores))
+        sorted_scores = [scores[i] for i in sorted_indices]
+        sorted_classes = [classes[i] for i in sorted_indices]
+        P = float(classes.count(1))
+        N = float(classes.count(0))
+        FP = 0
+        TP = 0
+        #
+        roc = {"fpr":[], "tpr":[], "precision":[], "FP":[], "TP":[],
+            "recall":[], "accuracy":[], "Fmeasure":[], 'threshold':[]}
+        fprev = 1.1 # scores are less than 1 
+
+        # an efficient algorithm to compute ROC based on T.Fawcett Pattern
+        # Recognition Letters 27, 2006
+        i = 0
+        while i<len(scores):
+            cls = sorted_classes[i]
+            score = sorted_scores[i]
+
+            if score != fprev and cls!=None:
+                thisroc = self._compute_fpr_tpr(FP, TP, N, P)
+                for key in ['fpr', 'tpr', "FP", "TP"]:
+                    roc[key].append(thisroc[key])
+                roc['threshold'].append(fprev)
+                fprev = score
+
+            if cls == 1:
+                TP += 1
+            elif cls == 0:
+                FP += 1
+            elif cls == None or np.isnan(cls):
+                pass
+            else:
+                raise ValueError("found a class value different from 0,1 or None")
+            i += 1
+        sroc = self._compute_fpr_tpr(FP, TP, N, P)
+        for key in ['fpr', 'tpr', "FP", "TP"]:
+            roc[key].append(thisroc[key])
+
+        # force the last value of tpr/fpr to be one
+        # TODO this is a hack to bebahve correctly when the final fpr/tpr are nt
+        # (1,1) as it should be.
+        roc['fpr'][-1] = 1
+        roc['tpr'][-1] = 1
+
+        roc['threshold'].append(sorted_scores[-1])
+
+        roc = self._compute_other_metrics(roc)
+        return roc
+
+    def compute_auc(self, roc=None):
+        """Compute AUC given a ROC data set (recomputed otherwise)
+
+        :param str roc: The roc data structure must be a dictionary with "tpr"
+            key. Could be a variable returned by :meth:`get_roc`. If not
+            provided, compute the ROC.
+        :return: the AUC
+
+        """
+        if roc == None:
+            roc = self.get_roc()
+        import scipy.integrate
+        value = scipy.integrate.trapz(roc['tpr'], roc['fpr'])
+        return value
+        #return sum(roc['tpr']) / len(roc['tpr'])
+
+    def compute_aupr(self, roc=None):
+        """Compute AUPR given a ROC data set (recomputed otherwise)
+
+        :param str roc: The roc data structure must be a dictionary with "tpr"
+            key. Could be a variable returned by :meth:`get_roc`. If not
+            provided, compute the ROC.
+        :return: the AUPR
+        """
+        if roc == None:
+            roc = self.get_roc()
+        import scipy.integrate
+        value = scipy.integrate.trapz(roc['precision'], x=roc['recall'])
+        return value
+
+    def _compute_other_metrics(self, roc):
+        #Be aware that there is alway a first value TP=0,FP=0
+        # this should be handled with care the recall/precision computatio
+        # This first value is used to compute fpr and tpr but should not be used 
+        # here
+        FP = np.array([float(x) for x in roc['FP']])
+        TP = np.array([float(x) for x in roc['TP']])
+        N = max(FP) + max(TP)
+
+        # The first value of precision is undefined since FP+TP=0
+        # It does not matter since it will be overwritten as explained here
+        # below
+
+        # TODO: to prevent warning, set FP[0] to 100.
+        FP[0] = 100
+        precision = TP / (FP + TP)
+        recall = TP/float(max(TP))
+        # The second value may also be be an issue. There are 2 cases.
+        # 1. TP>0
+        # 2. TP == 0 
+        # In the second case,  TP=0 and FP==0 should not happen. FP > 0 instead.
+        # In which case, recall = 0 and precision=0. No ambiguity
+        # In the first case, if TP>0 then precision >0 and recall > 0. 
+        # Therefore we end up without a defined value at recall==0
+        # So we should set a recall=0 for which precision will be the same as
+        # the one where recall >0
+        # somehow, precision and recall 
+        if TP[1]>0:
+            precision[0] = precision[1] 
+        else:
+            precision[0] = 0
+
+        # TODO
+        #roc['accuracy'] = (TP+FP)/(P+N)
+        #if roc['recall'] >0 and roc['precision'] != np.nan and roc['precision'] > 0 :
+        #    roc['Fmeasure'] = 2./(1./roc['precision']+1./roc['recall'])
+        #else:
+        #    roc['Fmeasure'] = np.nan
+        roc['precision'] = [float(x) for x in precision]
+        roc['recall'] = [float(x) for x in recall]
+        return roc
+
+
+    def plot_roc(self, roc=None):
+        """Plot ROC curves
+
+        .. plot::
+            :include-source:
+            :width: 80%
+
+            from dreamtools import rocs
+            r = rocs.ROC()
+            r.scores = [.9,.5,.6,.7,.1,.2,.6,.4,.7,.9, .2]
+            r.classes = [1,0,1,0,0,1,1,0,0,1,1]
+            r.plot_roc()
+
+        """
+        if roc == None:
+            roc = self.get_roc()
+        from pylab import plot, xlim, ylim ,grid, title, xlabel, ylabel
+        x = roc['fpr']
+        plot(x, roc['tpr'], '-o')
+        plot([0,1], [0,1],'r')
+        ylim([0, 1])
+        xlim([0, 1])
+        grid(True)
+        title("ROC curve (AUC=%s)" % self.compute_auc(roc))
+        xlabel("FPR")
+        ylabel("TPR")
+
+
+
+
+
---
+++ dreamtools/core/sageutils.py
@@ -0,0 +1,276 @@
+"""A module dedicated to synapse
+
+The class :class:`SynapseClient` is a specialised class built upon synapseclient
+package, which source code is on GIT::
+ 
+    git clone git://github.com/Sage-Bionetworks/synapsePythonClient.git
+    cd synapsePythonClient
+    python setup.py install
+
+When I started to build dream8 synapse pages and leaderboards, the synapseclient
+was still in development. In order to test the system, I built this class
+(SynapseClient) to add high level functionalities. These functionalities have
+been included later in the synapseclient. After successive cleanups, I've still
+have a couple of methods that may be redundant with synapse or not...
+
+::
+
+    >>> from dreamtools import sageutils
+    >>> s = sageutils.SynapseClient()
+
+
+"""
+import synapseclient
+from synapseclient import entity, Submission, SubmissionStatus
+
+
+__all__ = ["SynapseClient", "Login"]
+
+
+class Login(object):
+    """A simple class to login to synapse
+
+    :param client: Connection to synapse takes a
+        couple of seconds. This may be too much if in a debugging mode or
+        accessing to synapse from different places. The login can be instantiate
+        with an existing instance of SynapseClient, if which case, the instance
+        creation is fast. Otherwise, the default behaviour is to create a new
+        connection.
+
+    :: 
+
+        >>> from dreamtools.sageutils import Login
+        >>> l = Login()
+        This is a SynapseClient built on top of Synapse class. 
+        Trying to login automatically. 
+        Welcome, *****************
+        You're logged in Synapse
+        Welcome, cokelaer@ebi.ac.uk!
+
+        In [10]: l = sageutils.Login(l)
+
+
+
+    """
+    def __init__(self, client=None):
+        if client == None:
+            self.client = SynapseClient()
+            #self.client.login()
+        else:
+            self.client = client
+
+# note that synapseclient.Synapse does not inherit from object, hence the
+# additional base class here below that contains Synapse and object as base
+# classes
+class SynapseClient(synapseclient.Synapse, object):
+    """This class inherits all methods from synapseClient. 
+
+    Be aware that most of the functionalities are now available in synapseclient
+    itself. So, most of the methods that were written are hidden (double
+    underscore) and may be removed in the future. 
+
+    The only remaining feature is the automatic login, and simple version of the
+    downloadSubmission method. There is also a :meth:`json` method used
+    throughout the dream8hpn code.
+
+    """
+    def __init__(self, username=None, password=None):
+        """.. rubric:: Constructor
+
+        :param username: your synapse usename
+        :param password: your synapse password
+
+        You can create create a file called **.synapseConfig** (note the dot)
+        in your home directory and add something like::
+
+            [authentication]
+            username: yourlogin
+            password: yourpassword
+
+
+        """
+        super(SynapseClient, self).__init__()
+        # TODO: should be moved to Login ??
+        print("This is a SynapseClient built on top of Synapse class. ")
+        print("Trying to login automatically. ")
+        try:
+            self.login(username, password)
+            print("You're logged in Synapse")
+        except Exception, e:
+            print(e)
+            print("Failed to login automatically. Please try manual login")
+            raise Exception
+
+    def __getDataPath(self, entity, version=1):
+        """
+
+        obsolet .to be deleted. Feb 2014
+
+        """
+        try:
+            eid = entity['id']
+            e = self.get(eid, version=version, downloadFile=True)
+        except:
+            e = self.get(entity, version=version, downloadFile=True)
+        return e['path']
+
+    def downloadSubmissionAndFilename(self, sub, downloadFile=True, **kargs):
+        """Return filename of a submission downloaded from synapse. 
+
+        :param sub:  A submission (as a dictionary).
+        :param version:  The specific version to get. Defaults to the most recent version.
+        :param downloadFile: Whether associated files(s) should be downloaded.  
+             Defaults to True. If set to False, downloadLocation and ifcollision are ignored
+        :param downloadLocation: Directory where to download the Synapse File Entity.
+            Defaults to the local cache.
+        :param ifcollision: Determines how to handle file collisions.
+            May be "overwrite.local", "keep.local", or "keep.both".
+            Defaults to "keep.both".
+
+        .. warning:: ifcollision does not seem to work (0.5.1)
+
+        """
+
+        if isinstance(sub, dict) == False:
+            raise TypeError("input must be a submission (dictionary)")
+            
+
+        if downloadFile == False:
+            filename = self.getSubmission(sub, downloadFile=False)['filePath']
+        else:
+            filename = self.getSubmission(sub, downloadFile=True, **kargs)['filePath']
+
+        return filename
+
+
+    def __setProvenance(self, entity, name):
+        """Set provenance
+
+        :param entity: a valid entity object
+        :param name: name to set on an activity
+
+        ::
+
+            s.setProvenance("syn375811", name="test")
+
+        .. warning:: may be obsolet. Look into original synapseclient instead
+        """
+        activity = synapseclient.Activity(name=name)
+        super(SynapseClient, self).setProvenance(entity, activity)
+
+    def __getEvaluation(self, eid):
+        """Returns an evaluation
+
+        obsolet available in synapseclient now"""
+        e = self.restGET("/evaluation/%s" % eid)
+        return e
+
+    def __getEvaluationByName(self, name):
+        """Get evaluation by its name
+        
+        
+        
+        .. warning:: may be obsolet. Look into original synapseclient instead
+        """
+        e = self.restGET("/evaluation/name/%s" % name)
+        return e
+
+    def __createEvaluation(self, name, status="PLANNED", contentSource=None):
+        """
+
+        >>> createEvaluation("testval1", status="PLANNED", contentSource="")
+
+        .. warning:: may be obsolet. Look into original synapseclient instead
+        """
+        if contentSource == None:
+            raise ValueError("contentSource must be a valid wiki id")
+        evaluation = synapseclient.Evaluation(name=name, status=status,
+            contentSource=contentSource) # e.g., 1720047 for HPN
+
+        print("Creating evaluation (POST)")
+        try:
+            res = self.restPOST("/evaluation", body=self.json(evaluation))
+            return res
+        except Exception, e:
+            print(e)
+            print("Evaluation not created. Exist already ?")
+            raise Exception
+
+    def __deleteEvaluation(self, eid):
+        """Deletes an evaluation page
+
+        """
+        print("deleting evaluation eid")
+        res = self.restDELETE("/evaluation/" + eid)
+        return res
+
+    def __createWiki(self, owner, title, markdown, owner_type=None):
+        """
+
+        :param owner: the owner object (entity, competition, evaluation) 
+            with which the new wiki page will be associated
+        :param markdown: the contents of the wiki page in markdown
+        :param owner_type: if not provided, the client tries to figure out the type.
+            e.g., Works for evaluation 
+        """
+        try:
+            res = self._createWiki(owner, title, markdown, owner_type=owner_type)
+            return res
+        except Exception, e:
+            print("Could not create the wiki. Exists already ?")
+            raise Exception
+
+    def __getWiki_TC(self, owner, owner_type):
+        """Returns wiki given owner and owner_type
+
+        """
+        res = self.restGET("/%s/%s/wiki" % (owner_type, owner['id']))
+        return res
+
+    def __createWikiChild(self, owner, title):
+        # retrieve the main wiki
+        wiki = self.getWiki_TC(owner, "evaluation")
+        wikiURI = "/evaluation/%s/wiki" % owner['id']
+
+        print(wikiURI)
+        wikiChild = synapseclient.Wiki(title=title, markdown="", parentWikiId=wiki['id'])
+        res = self.restPOST(wikiURI, wikiChild.getjson())
+
+        return res
+
+    def getMyProfile(self):
+        """Returns user profile"""
+        return self.restGET("/userProfile")
+
+    def __joinEvaluation(self, evalId):
+        """Join an existing evaluation
+
+        :param evalIe: a valid evaluation identifier
+        """
+        prof =  self.getMyProfile()
+        try:
+            self.restPOST("/evaluation/%s/participant/%s" % (evalId, prof['ownerId']), list())
+        except Exception, e:
+            print("Joining evaluation failed. Maybe you've already joined.")
+
+    def json(self, data):
+        """Transform relevant object into json object"""
+        import json
+        data = json.dumps(data)
+        return data
+
+
+def __time2datetime(timedate):
+    """not used anymore ???"""
+    import datetime
+
+    date = [int(x) for x in timedate.split("T")[0].split("-")]
+    time = timedate.split("T")[1].split(":")
+    print time
+    time[2] = time[2].split(".")[0]
+    time = [int(x) for x in time]
+    this = datetime.datetime(date[0], date[1], date[2], time[0], time[1], time[2])
+    return this
+    
+
+
