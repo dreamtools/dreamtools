7f721eac950ccf3756479abfb1ced6ac9210a2c0
Thomas Cokelaer <cokelaer@gmail.com>
2015-06-12 14:54:33 +0100

    update test.

--- test/dream5/test_d5c2.py
+++ test/dream5/test_d5c2.py
@@ -1,1 +1,15 @@
 from dreamtools import D5C2
+from nose.tools import assert_almost_equal
+import os
+
+
+def test_d5c2():
+    # load data first
+    s = D5C2(Ntf=2)  # temporary directory will be created
+    s.download_templates()
+    filename = s.directory + os.sep + 'templates.txt.gz'
+    s.score(filename)
+    v = s.get_table().ix[20].Pearson
+    assert_almost_equal(v, 0.51295034)
+
+    s.cleanup()
