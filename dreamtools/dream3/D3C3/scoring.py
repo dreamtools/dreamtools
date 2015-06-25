5b00cadea2c8e3a2aa251ed978af37c464e6927f
cokelaer <cokelaer@gmail.com>
2015-06-25 13:17:42 +0100

    remove asserts

--- dreamtools/dream3/D3C3/scoring.py
+++ dreamtools/dream3/D3C3/scoring.py
@@ -6,6 +6,8 @@ Robert Prill).
 import os
 from dreamtools.core.challenge import Challenge
 import pandas as pd
+import numpy as np
+
 
 class D3C3(Challenge):
     """A class dedicated to D3C3 challenge
@@ -58,69 +60,74 @@ class D3C3(Challenge):
         self.G = self._read_challenge(self.download_goldstandard())
         self.T = self._read_challenge(filename)
 
-        from scipy.stats.stats import pearsonr
+        from scipy.stats.stats import pearsonr, spearmanr
         
         G = self.G[self.G.columns[2:]].values
         T = self.T[self.T.columns[2:]].values
-        # column correlation
-        data = [pearsonr(G[i,:], T[i,:]) for i in range(0,50)]
-        rho_col = [x[0] for x in data]
-        pval_col = [x[0] for x in data]
-        # row correlation
-        data = [pearsonr(G[:,i], T[:,i]) for i in range(0,8)]
+
+        # Using scipy, the pvalue are not the same as in matlab for several reasons.
+        # first cipy returs only 2-tail pvalue but more importantly, it is
+        # a rough approximation as mentionned in their doc and when compared
+        # to matlab differences can be large. So, we use R, which results are
+        # also differnt but much close (1-2% different
+        """data = [spearmanr(G[i,:], T[i,:]) for i in range(0,50)]
         rho_row = [x[0] for x in data]
-        pval_row = [x[0] for x in data]
+        pval_row = [x[1] for x in data]
+        # row correlation
+        data = [spearmanr(G[:,i], T[:,i]) for i in range(0,8)]
+        rho_col = [x[0] for x in data]
+        pval_col = [x[1] for x in data]
+        """
+
+        from dreamtools.core.rtools import RTools
+        rtool = RTools(verboseR=False)
+        pval_row = []
+        pval_col = []
+        rho_row = []
+        rho_col = []
+
+        for i in range(0,50):
+            rtool.session.t = T[i,:].copy()
+            rtool.session.g = G[i,:].copy()
+            rtool.session.run("results = cor.test(t, g, method='spearman', alternative='greater', exact=F)")
+            rho_row.append(rtool.session.results['estimate'])
+            pval_row.append(rtool.session.results['p.value'])
+        for i in range(0, 8):
+            rtool.session.t = T[:,i].copy()
+            rtool.session.g = G[:,i].copy()
+            rtool.session.run("results = cor.test(t, g, method='spearman', alternative='greater', exact=F)")
+            rho_col.append(rtool.session.results['estimate'])
+            pval_col.append(rtool.session.results['p.value'])
+
+
+
+
+        print("""
+WARNING: the spearman correlation pvalue are computed using R. Pvalues are slightly different from
+those computed using matlab and therefore the final values may differ by a few percents to
+the pvlues reported in the original challenge. """)
+
+
 
         self.rho_col = rho_col
         self.pval_col = pval_col
-        
-        """
+        self.rho_row = rho_row
+        self.pval_row = pval_row
 
-for ri = 1:size(G,2)
-	g = G(:,ri);
-	t = T(:,ri);
-	[rho, pval] = corr(g,t,'type','Spearman','tail','gt');  % rho greater than zero
-	rho_col(ri) = rho;
-	pval_col(ri) = pval;
-end
+        #assert int(sum(rho_row)*1000000)/1000000. == 2.105420
+        #assert int(sum(rho_col)*1000000)/1000000. == -0.406626
 
+        #this differs from matlab
+        score1 = np.exp(np.nansum(np.log(pval_row))/50)
+        # this is exact as in matlab
+        score2 = np.exp(np.nansum(np.log(pval_col))/8.)
 
-%% row correlation
-for ci = 1:size(G,1)
-	g = G(ci,:)';
-	t = T(ci,:)';
-	[rho, pval] = corr(g,t,'type','Spearman','tail','gt');  % re-ranks before computing corr coeff 
-	rho_row(ci) = rho;
-	pval_row(ci) = pval;
-end
-%% pretty names
-%% gene profile:  all genes at a given time-point (a col)
-%% time profile:  a single gene across all times (a row)
-rho_gene_profile = rho_col;
-rho_time_profile = rho_row;
-pval_gene_profile = pval_col;
-pval_time_profile = pval_row;
-%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% summary statistics
-%% overall pval is the geometric mean
-N = length(pval_time_profile);
-overall_time_profile_pval = exp(sum(log(pval_time_profile))/N);
-
-N = length(pval_gene_profile);
-overall_gene_profile_pval = exp(sum(log(pval_gene_profile))/N);
-
-%% SCORE
-score = sum(-log10([ overall_time_profile_pval overall_gene_profile_pval ]))/2;
-
-        """
+        score = sum(-np.log10([score1, score2]))/2.
+        return {'score': score}
 
     def download_template(self):
-        return self._pj([self._path2data, 'template', 'D3C3_template.txt'])
-
+        return self._pj([self._path2data, 'templates', 'D3C3_template.txt'])
 
     def _read_challenge(self, filename):
-        df = pd.read_csv(filename, sep='\t')
-        return df
-
-
-
+       df = pd.read_csv(filename, sep='\t')
+       return df
