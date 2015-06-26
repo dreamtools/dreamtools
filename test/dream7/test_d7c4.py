66adaf23af6dfea2dbcc9c9a7d639e260eb65e14
cokelaer <cokelaer@gmail.com>
2015-06-26 15:45:41 +0100

    fix test for d7c4

--- test/dream7/test_d7c4.py
+++ test/dream7/test_d7c4.py
@@ -3,12 +3,7 @@ from nose.tools import assert_almost_equal
 
 
 def test_d7c4():
-
-
     s = D7C4()
-    filename = s.download_template()     
-    df = s.score(filename) 
-
-
+    filename = s.download_template()
+    df = s.score(filename)
     assert_almost_equal(df['c-index']['probabalistic c-index'], 0.578443)
-
