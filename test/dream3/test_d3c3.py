08f422be5bc81b75dbb9d27708d4f8675aa54187
cokelaer <cokelaer@gmail.com>
2015-06-26 15:44:17 +0100

    add new test.

---
+++ test/dream3/test_d3c3.py
@@ -0,0 +1,12 @@
+from dreamtools import D3C3
+from nose.tools import assert_almost_equal
+
+
+def test_d3c1():
+    # load data first
+    s = D3C3()  # temporary directory will be created
+    filename = s.download_template()
+    res = s.score(filename)
+
+    assert_almost_equal( res['score'], 0.3266163)
+
