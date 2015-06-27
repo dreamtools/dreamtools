21632703f2e6b2c4c26737d285f8c4cbad98ac7b
Thomas Cokelaer <cokelaer@gmail.com>
2015-06-27 11:55:42 +0100

    fix test.

--- test/dream5/test_d5c1.py
+++ test/dream5/test_d5c1.py
@@ -1,9 +1,10 @@
 from dreamtools import D5C1
 from nose.tools import assert_almost_equal
 
+
 def test_d5c1():
 
     s = D5C1()
     filename = s.download_template()
-    res = s.score_prediction(filename)
+    res = s.score(filename)
     assert_almost_equal(res['score'], 0.0001266)
