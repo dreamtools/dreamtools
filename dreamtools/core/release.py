76f82299dd505735080cd2028ef6dc2e4c4a8247
Thomas Cokelaer <cokelaer@gmail.com>
2015-07-01 13:49:45 +0100

    add new files for future usage.

---
+++ dreamtools/core/release.py
@@ -0,0 +1,16 @@
+"""
+
+Mechanism to download all required files from Synapse, create a bundle
+, post if on github or elsewhere to not rely on synpase connection ?
+
+
+"""
+
+
+
+
+class Release(object):
+
+
+    def __init__(self):
+        pass
