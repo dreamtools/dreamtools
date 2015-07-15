644675cb6aec14a18d75cf8eb85c00b0f6781121
cokelaer <cokelaer@gmail.com>
2015-07-15 17:44:10 +0100

    Fixing D3C4 to take the 5 networks altogether rather than just one.

--- dreamtools/dream3/D3C4/scoring.py
+++ dreamtools/dream3/D3C4/scoring.py
@@ -1,14 +1,14 @@
 """
 
 Implementation in Python from Thomas Cokelaer.
-Original code in matlab (Gustavo Stolovitzky and Robert Prill, Bernd Jagla).
+Original code in matlab (Gustavo Stolovitzky and Robert Prill).
 
 """
 import os
 from dreamtools.core.challenge import Challenge
 import pandas as pd
 from dreamtools.core.rocs import D3D4ROC
-
+import numpy as np
 
 class D3C4(Challenge, D3D4ROC):
     """A class dedicated to D3C4 challenge
@@ -18,12 +18,12 @@ class D3C4(Challenge, D3D4ROC):
 
         from dreamtools import D3C4
         s = D3C4()
-        filename = s.download_template() 
-        s.score(filename) 
+        filename = s.download_template()
+        s.score(filename)
 
 
-    .. note:: AUROC/AUPR and p-values are returned for a 
-        given sub-challenge. In the DREAM LB, the 5 networks 
+    .. note:: AUROC/AUPR and p-values are returned for a
+        given sub-challenge. In the DREAM LB, the 5 networks
         are combined together. We should have same implemntatin
         as in D4C2
 
@@ -36,10 +36,8 @@ class D3C4(Challenge, D3D4ROC):
         self._path2data = os.path.split(os.path.abspath(__file__))[0]
         self._init()
 
-        self.sub_challenges = []
-        for x in ['10', '100', '50']:
-            for y in ['Yeast3', 'Yeast1', 'Yeast2', 'Ecoli2', 'Ecoli1']:
-                self.sub_challenges.append(x + '_' + y)
+        self.sub_challenges = ['10', '100', '50']
+        self.netnames = ['Yeast3', 'Yeast1', 'Yeast2', 'Ecoli2', 'Ecoli1']
 
     def _init(self):
         # should download files from synapse if required.
@@ -83,36 +81,98 @@ class D3C4(Challenge, D3D4ROC):
         self._download_data('%s_InSilicoSize50_Yeast2.txt' % tag, 'syn4558554')
         self._download_data('%s_InSilicoSize50_Yeast3.txt' % tag, 'syn4558556')
 
-    def score(self, filename, subname=None):
-        name1, name2 = self._check_subname(subname)
-        results =  self.score_prediction(filename, subname)
-        AUC, AUROC, prec, rec, tpr, fpr, p_auroc, p_aupr = results
-        return {'AUROC': AUROC, 'AUC':AUC, 'p_auroc':p_auroc, 'p_aupr':p_aupr}
+    def _check_filename(self, filename):
+        end = filename[-10:]
+        if end[6] != '.':
+            raise ValueError("File must end with a suffix of 3 letters e.g. .csv")
+        end = end[0:6]
 
-    def _check_subname(self, subname):
-        name1, name2 = subname.split("_")
-        names = ['Ecoli1', 'Ecoli2', 'Yeast1', 'Yeast2', 'Yeast3']
-        error = "The sub challenge name must be X_Y where X is in 10 50 100"
-        error += "\nand Y is in  %s" % names
-        if name1 not in ['100', '10', '50']:
-            raise ValueError(error)
-        if name2 not in names:
-            raise ValueError(error)
-        return name1, name2
+        endings = ['Ecoli1', 'Ecoli2', 'Yeast1', 'Yeast2', 'Yeast3']
+        if end not in endings:
+             raise ValueError("File must end with a valid batch value in %s" %
+                     endings)
+        return end
+
+    def score(self, filename, subname):
+        subname = str(subname)
+        self._check_subname(subname)
+
+        if isinstance(filename, str):
+            end = self._check_filename(filename)
+            if end not in subname:
+                subname += "_"+end
+            results =  self.score_prediction(filename, subname)
+            del results['tpr']
+            del results['fpr']
+            del results['rec']
+            del results['prec']
+            return results
+            #{'AUROC': AUROC, 'AUC':AUC, 'p_auroc':p_auroc,
+            #            'p_aupr':p_aupr}
+        elif isinstance(filename, list):
+            assert len(filename) == 5, "if a list of filenames is provide, it must contain 5 names"
+            results = {}
+            for i, this in enumerate(filename):
+                end = self._check_filename(this)
+                results['Net%s' % i] = self.score_prediction(this,
+                        subname+"_"+end)
+
+            df = pd.DataFrame(results).T
+            # get rid of non important data
+            df = df[['AUROC', 'AUPR', 'p_aupr', 'p_auroc']]
+            df = df.astype('float64')
+
+            final_score =  -np.mean(np.log10(df[['p_auroc', 'p_aupr']]))
+
+            results = {}
+            results['AUPR_SCORE'] = final_score['p_aupr']
+            results['AUROC_SCORE'] = final_score['p_auroc']
+            overall_score = np.mean(final_score)
+
+            for index in df.index:
+                results['%s_AUROC' % index] = df.ix[index]['AUROC']
+            for index in df.index:
+                results['%s_AUPR' % index] = df.ix[index]['AUPR']
+
+            final_score = 10**-(final_score)
+            results['AUPR_PVAL'] = final_score['p_aupr']
+            results['AUROC_PVAL'] = final_score['p_auroc']
+
+            results['SCORE'] = overall_score
+            results = pd.TimeSeries(results)
+
+            results = results[['SCORE', 'AUPR_PVAL', 'AUPR_SCORE', 'AUROC_PVAL',
+                'AUROC_SCORE','Net1_AUPR',  'Net2_AUPR', 'Net3_AUPR',
+                'Net4_AUPR', 'Net5_AUPR', 'Net1_AUROC', 'Net2_AUROC',
+                'Net3_AUROC', 'Net4_AUROC', 'Net5_AUROC']]
+            return results
+
+    def _warning(self):
+        print("\nNote that 5 networks had to be submitted")
+        print("You can score an individual one.")
+        print("Or provide a list of 5 filenames.")
+        print("You can use a wildcard * or be explicit.")
+        print("The file filenames must end in Yeast1, Yeast2, Yeast3,  Ecoli1, Ecoli2")
+        print("filename for th Yeast1 case only.")
 
     def download_template(self, subname):
-        name1, name2 = self._check_subname(subname)
-        if name1 == '10':
-            filename = self._pj([self._path2data, 'templates',
-                'example_InSilicoSize%s_%s.txt' % (name1, name2)])
-        else:
-            # no template/examples for those cases were provided
-            filename = self.download_goldstandard(subname)
+        # no template per se, so we return GS
+        # self._check_subname(subname)
+        self._warning()
+        filename = self.download_goldstandard(subname)
         return filename
 
     def download_goldstandard(self, subname):
-        name1, name2 = self._check_subname(subname)
-        return self.get_pathname('DREAM3GoldStandard_InSilicoSize%s_%s.txt' % (name1, name2))
+        subname = str(subname)
+        if subname in self.sub_challenges:
+            subname2 = subname + "_Yeast1"
+        else:
+            subname2, netname = subname.split("_", 1)
+            self._check_subname(subname2)
+            subname2 = subname
+
+        return self.get_pathname('DREAM3GoldStandard_InSilicoSize%s.txt' %
+                subname2)
 
     def _load_network(self, filename):
         df = pd.read_csv(filename, header=None, sep='[ \t]', engine='python')
@@ -130,9 +190,10 @@ class D3C4(Challenge, D3D4ROC):
         :return:
 
         """
-        name1, name2 = self._check_subname(subname)
+        name1, name2 = subname.rsplit("_",1)
+
         gs_filename = self.download_goldstandard(subname)
-        pdf_filename = self.get_pathname("PDF_InSilicoSize%s_%s.mat" % (name1, name2))
+        pdf_filename = self.get_pathname("PDF_InSilicoSize%s.mat" % subname)
 
         self.gold_data = self._load_network(gs_filename)
         self.test_data = self._load_network(filename)
@@ -144,11 +205,17 @@ class D3C4(Challenge, D3D4ROC):
         #test = list(newtest['2_x'])
         gold_index = list(newtest['2_y'])
 
-        AUC, AUROC, prec, rec, tpr, fpr = self.get_statistics(self.gold_data, self.test_data, gold_index)
-        p_auroc = self._probability(self.pdf_data['x_auroc'][0], self.pdf_data['y_auroc'][0], AUROC)
-        p_aupr = self._probability(self.pdf_data['x_aupr'][0], self.pdf_data['y_aupr'][0], AUC)
+        AUC, AUROC, prec, rec, tpr, fpr = self.get_statistics(self.gold_data,
+                self.test_data, gold_index)
 
-        return AUC, AUROC, prec, rec, tpr, fpr, p_auroc, p_aupr
+        p_auroc = self._probability(self.pdf_data['x_auroc'][0],
+                self.pdf_data['y_auroc'][0], AUROC)
+        p_aupr = self._probability(self.pdf_data['x_aupr'][0],
+                self.pdf_data['y_aupr'][0], AUC)
+
+        return {'AUPR': AUC, 'AUROC':AUROC, 'prec':prec,
+                'rec':rec, 'tpr':tpr, 'fpr':fpr,
+                'p_auroc':p_auroc, 'p_aupr':p_aupr}
 
     def _probability(self, X, Y, x):
         dx = X[2] - X[1]
