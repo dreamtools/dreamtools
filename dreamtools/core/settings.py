b55c6c043e3c0292968fad8765eeccdfa740d6d6
cokelaer <cokelaer@gmail.com>
2015-09-14 12:22:57 +0100

    use CustomConfig from easydev to handle files in config directory

--- dreamtools/core/settings.py
+++ dreamtools/core/settings.py
@@ -15,22 +15,17 @@
 ##############################################################################
 """Tools to handle a configuration file."""
 import os
-import appdirs
+
+from easydev import CustomConfig
 
 
 __all__ = ["DREAMToolsConfig"]
 
 
-# first item if the value
-# second item if a type or TUPLE of types possible
-# third item is documentation
-
-class DREAMToolsConfig(object):
+class DREAMToolsConfig(CustomConfig):
 
     def __init__(self, verbose=False):
-        self.verbose = verbose
-        #super(DREAMToolsConfig, self).__init__(name="dreamtools",
-        #        default_params=defaultParams)
+        super(DREAMToolsConfig, self).__init__("dreamtools", verbose)
         if self.verbose is True:
             print("Welcome to DREAMTools")
             print("=====================")
@@ -40,43 +35,6 @@ class DREAMToolsConfig(object):
             print(" >>> filename = c.download_template()")
             print(" >>> c.score(filename)\n")
             print(" Any issues/suggestions ? Visit http://github.com/dreamtools/\n\n")
-        self.appdirs = appdirs.AppDirs("dreamtools")
-
-    def _get_config_dir(self):
-        sdir = self.appdirs.user_config_dir
-        return self._get_and_create(sdir)
-    user_config_dir = property(_get_config_dir,
-            doc="return directory of this configuration file")
-    def _get_and_create(self, sdir):
-        if not os.path.exists(sdir):
-            print("Creating directory %s " % sdir)
-            try:
-                self._mkdirs(sdir)
-            except Exception:
-                print("Could not create the path %s " % sdir)
-                return None
-        return sdir
-
-    def _mkdirs(self, newdir, mode=0o777):
-        """from matplotlib mkdirs
-
-        make directory *newdir* recursively, and set *mode*.  Equivalent to ::
-
-        > mkdir -p NEWDIR
-        > chmod MODE NEWDIR
-        """
-        try:
-            if not os.path.exists(newdir):
-                parts = os.path.split(newdir)
-                for i in range(1, len(parts) + 1):
-                    thispart = os.path.join(*parts[:i])
-                    if not os.path.exists(thispart):
-                        os.makedirs(thispart, mode)
-
-        except OSError as err:
-            # Reraise the error unless it's about an already existing directory
-            if err.errno != errno.EEXIST or not os.path.isdir(newdir):
-                raise
 
 
 
