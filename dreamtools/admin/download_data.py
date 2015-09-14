6e2fc1ae7135d39a0d92250dc786e3e8c75d635f
cokelaer <cokelaer@gmail.com>
2015-09-14 19:47:55 +0100

    Create tool to easily create a bundle of all the files in the dreamtpath directory.

--- dreamtools/admin/download_data.py
+++ dreamtools/admin/download_data.py
@@ -1,3 +1,6 @@
+import os
+import tarfile
+
 from dreamtools import Challenge
 import dreamtools
 
@@ -27,14 +30,29 @@ def _generic_download(name, mode):
 
 
 def download_gs(name):
+    """Download GS file(s) for a given challenge
+
+    :param name: a valid nickname e.g. D2C1"""
     _generic_download(name, 'gs')
 
 
 def download_template(name):
+    """Download template file(s) for a given challenge
+
+    :param name: a valid nickname e.g. D2C1"""
     _generic_download(name, 'template')
 
 
 def download_all():
+    """This function downloads all templates and GS stored in Synapse
+
+    This could be used to create a ZIP file will all downlable files and
+    use to provide a bundle of those files for those who do not wish to
+    have a Synapse access.
+
+    This could be useful for testing as well.
+
+    """
     names = get_challenge_list()
     for name in names:
         print("Downloading template for %s" % name)
@@ -52,5 +70,22 @@ def download_all():
             pass
         except Exception as err:
             raise(err)
+
+
+class DREAMToolsBundle(object):
+    def __init__(self, verbose=True):
+        self.verbose = True
+        self.download_all()
+
+    def download_all(self):
+        download_all()
+
+    def create_bundle(self, output_filename, source_dir=None):
+        if source_dir is None:
+            from dreamtools import dreampath
+            source_dir = dreampath
+        with tarfile.open(output_filename, "w:gz") as tar:
+            if self.verbose:
+                tar.add(source_dir, arcname=os.path.basename(source_dir))
 
 
