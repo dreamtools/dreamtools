8025ebfef582caab3e7c0a34f5c419373e3fbf01
cokelaer <cokelaer@gmail.com>
2015-09-15 13:23:59 +0100

    add new test file.

---
+++ test/core/test_ziptools.py
@@ -0,0 +1,12 @@
+from dreamtools import ZIP
+
+
+# most of the ZIP class is tests in various challenges
+# Here, we just want to test things that fails.
+
+def test_zip():
+    z = ZIP()
+    try:
+        z.loadZIPFile('p')
+    except:
+        pass
