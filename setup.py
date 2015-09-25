dc4a4db82ebea55a1d8c38b0218359b9f849c2f8
cokelaer <cokelaer@gmail.com>
2015-09-25 15:05:17 +0100

    create first release upgrading version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from Cython.Build import cythonize
 
 
 _MAJOR               = 0
-_MINOR               = 99
+_MINOR               = 1
 _MICRO               = 0
 version              = '%d.%d.%d' % (_MAJOR, _MINOR, _MICRO)
 release              = '%d.%d' % (_MAJOR, _MINOR)
