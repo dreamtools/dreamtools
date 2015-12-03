c63a4709391a789c3cb67a3af9a747285607a9ec
cokelaer <cokelaer@gmail.com>
2015-12-03 12:28:54 +0000

    add scikit-learn in setup

--- setup.py
+++ setup.py
@@ -85,7 +85,7 @@ setup(
 
     install_requires = ['cython', 'numpy', 'matplotlib', 'pandas', 'appdirs',
         'easydev>=0.9.6', 'fitter', 'synapseclient', 'tabulate', 'scipy',
-        'biokit','xlrd', 'numpexpr', 'sklearn'],
+        'biokit','xlrd', 'numexpr', 'scikit-learn'],
 
     ext_modules = cythonize(["dreamtools/dream8/D8C1/*.pyx"]),
 
