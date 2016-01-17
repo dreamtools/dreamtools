724e0e6aa6a159c31aee0188be45dde9c3f994a3
Thomas Cokelaer <cokelaer@gmail.com>
2016-01-17 22:35:34 +0100

    Update conda_install.bat

--- conda_install.bat
+++ conda_install.bat
@@ -1,33 +1,38 @@
-set CONDA_NAME=dreamtools_py2
+@echo off
 
-echo Creating a new conda environment for DREAMTools
-echo It will be called dreamtools_py2
-echo If it exists already, its creation will be skipped
+@echo: Creating a new conda environment for DREAMTools
+@echo: It will be called dreamtools_py2
+@echo: If it exists already, its creation will be skipped
 
-IF NOT EXIST anacondar\envs\dreamtools_py2 GOTO DIREXISTS
-conda create --name dreamtools_py2 python=2.7 
-:DIREXISTS
+IF NOT EXIST Anaconda2\envs\dreamtools_py2 conda create --name dreamtools_py2 python=2.7 
 
+call activate dreamtools_py2
 
-activate dreamtools_py2
-
-conda install numpy scipy cython matplotlib pandas scikit-learn gevent numpexp ipython
+conda install numpy scipy cython matplotlib pandas scikit-learn gevent numpexpr ipython
 
 
 pip install dreamtools
-
 pip install -U --no-deps
 
-set VERSION=1
-echo 
-echo 
-echo Version of DREAMTools installed: %VERSION%
-echo -n "Python version:"; echo " $(python --version)
-echo 
-echo PLEASE, close this terminal, open a new one and activate the conda environement, as follows:: 
-echo 
-echo    source activate %CONDA_NAME%
-echo 
-echo Once done, check that DREAMTools is available:\n\n
-echo     dreamtools --help
+dreamtools --version > version.txt
+set /p VERSION=<version.txt
+
+@echo off
+@echo Version of DREAMTools %VERSION%
+@echo:
+
+python --version>version.txt
+set /p VERSION=<version.txt
+@echo: %PYTHON%
+@echo:
+@echo PLEASE, close this terminal, open a new one and activate the conda environement, as follows:: 
+@echo:
+@echo:
+@echo:    activate dreamtools_py2
+@echo:
+@echo: Once done, check that DREAMTools is available:\n\n
+@echo:
+@echo    dreamtools --help
+
+pause
 
